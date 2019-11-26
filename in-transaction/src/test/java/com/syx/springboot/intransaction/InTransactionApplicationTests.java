8fb1d83f2ce591268338fdd8d8b2ed271b27e8d4
邵永鑫 <shaoyongxin@xiongmaocar.com>
2019-11-26 14:26:34 +0800

    feat: 新增in-transaction

---
+++ in-transaction/src/test/java/com/syx/springboot/intransaction/InTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.intransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
