cbfdc7bdcabb254845cc0214d0aad5e7319fa667
邵永鑫 <shaoyongxin@xiongmaocar.com>
2019-12-25 16:52:34 +0800

    feat: 新增消息队列模块

---
+++ in-mq/src/main/java/com/syx/springboot/inmq/InMqApplication.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.inmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMqApplication.class, args);
+    }
+
+}
---
+++ in-mq/src/test/java/com/syx/springboot/inmq/InMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.inmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
