ceaa8a63babaf2f13258bb14647402c8c007b288
邵永鑫 <shaoyongxin@xiongmaocar.com>
2019-12-27 16:44:33 +0800

    fix: 完善RocketMQ配置

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-integration
+integration 
 ##整合项目
 in-redis        整合redis
 in-apollo       整合apollo
 in-es           整合es
 in-transaction  整合transaction
+in-apollo       使用apollo配置中心
+in-eureka       使用注册中心
+in-mq           整合MQ消息队列
--- in-mq/src/main/java/com/syx/springboot/inmq/rocket/config/RocketMqConsumer.java
+++ in-mq/src/main/java/com/syx/springboot/inmq/rocket/config/RocketMqConsumer.java
@@ -1,11 +1,15 @@
 package com.syx.springboot.inmq.rocket.config;
 
 import com.aliyun.openservices.ons.api.Consumer;
+import com.aliyun.openservices.ons.api.ONSFactory;
+import com.aliyun.openservices.ons.api.PropertyKeyConst;
+import com.google.common.collect.Lists;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Properties;
 
 /**
  * 阿里云 RocketMQ 的消费者订阅
@@ -31,8 +35,40 @@ public class RocketMqConsumer {
     @Getter
     private List<Consumer> consumers;
 
-    public RocketMqConsumer build(){
+    /**
+     * 构建多个消费者
+     *
+     * @param
+     * @return
+     */
+    public RocketMqConsumer build() {
+        this.consumers = Lists.newArrayList();
+        for (RocketMqConsumerProperties consumerProperties : consumerPropertiesList) {
+            String groupId = consumerProperties.getGroupId();
 
-        return null;
+            // 链接配置
+            Properties properties = new Properties();
+            RocketMqProperties connProperties = consumerProperties.getProperties();
+            properties.setProperty(PropertyKeyConst.SecretKey, connProperties.getSecretKey());
+            properties.setProperty(PropertyKeyConst.AccessKey, connProperties.getAccessKey());
+            properties.setProperty(PropertyKeyConst.NAMESRV_ADDR, connProperties.getNameSrvAddr());
+            properties.setProperty(PropertyKeyConst.GROUP_ID, groupId);
+
+            // 绑定监听的 topic
+            Consumer consumer = ONSFactory.createConsumer(properties);
+            for (ConsumerDetail consumerDetail : consumerProperties.getConsumerDetailList()){
+                String topic = consumerDetail.getTopic();
+                String[] tags = consumerDetail.getTags();
+
+                // 绑定要监听的tag，多个tag用 || 隔开
+                consumer.subscribe(topic, StringUtils.join(tags, "||"), consumerDetail.getListener());
+                log.info("阿里云 启动消费者 GROUP_ID-{} TOPIC-{} 成功!", groupId, topic);
+            }
+
+            consumer.start();
+            this.consumers.add(consumer);
+        }
+        log.info("阿里云 启动{}个消费者。", this.consumers.size());
+        return this;
     }
 }
