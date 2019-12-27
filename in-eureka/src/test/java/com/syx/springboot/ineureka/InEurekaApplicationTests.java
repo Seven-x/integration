c5856837c0208733d47b07e29de5d20ea5f3a26f
邵永鑫 <shaoyongxin@xiongmaocar.com>
2019-12-27 09:34:10 +0800

    fix: 修改测试类引用错误

--- in-eureka/src/test/java/com/syx/springboot/ineureka/InEurekaApplicationTests.java
+++ in-eureka/src/test/java/com/syx/springboot/ineureka/InEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.syx.springboot.inutils;
+package com.syx.springboot.ineureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class InUtilsApplicationTests {
+class InEurekaApplicationTests {
 
     @Test
     void contextLoads() {
