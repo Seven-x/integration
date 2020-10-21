8d6ab8ecac46575e414780ffc431646b2b1e62bf
Seven-X <1098899894@qq.com>
2020-10-21 18:33:58 +0800

    feat: 添加maven 项目

---
+++ in-maven/src/main/java/com/syx/springboot/inmaven/inmaven/InMavenApplication.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.inmaven.inmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMavenApplication.class, args);
+    }
+
+}
